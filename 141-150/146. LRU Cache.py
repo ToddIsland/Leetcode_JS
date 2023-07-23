c8a9d0a738f3a9e8cf4cbaa68ec8a296b51bd2c1
Todd Hu <jkghdfas6@gmail.com>
2023-07-22 20:08:37 -0400

    Jul 22

--- 141-150/146. LRU Cache.py
+++ 141-150/146. LRU Cache.py
@@ -1,27 +1,45 @@
-class LRUCache:
+class Node:
+    def __init__(self, key, val) -> None:
+        self.key, self.val = key, val
+        self.prev = self.next = None
 
+
+class LRUCache:
     def __init__(self, capacity: int):
         self.cap = capacity
-        self.store = {}
-        self.q = collections.deque()
+        self.cache = {}
+        # left LRU right most recent
+        self.left, self.right = Node(0, 0), Node(0, 0)
+        self.left.next, self.right.prev = self.right, self.left
+
+    def remove(self, node):
+        prev, nxt = node.prev, node.next
+        prev.next, nxt.prev = nxt, prev
+
+    def insert(self, node):
+        prev = self.right.prev
+        prev.next, node.prev = node, prev
+        node.next, self.right.prev = self.right, node
 
     def get(self, key: int) -> int:
-        ret = self.store.get(key, -1)
-        if ret != -1:
-            if len(self.q) == self.cap:
-                
-        return ret
+        if key in self.cache:
+            self.remove(self.cache[key])
+            self.insert(self.cache[key])
+            return self.cache[key].val
+        return -1
 
     def put(self, key: int, value: int) -> None:
-        if key not in self.store:
-            if len(self.q) == self.cap:
-                temp = self.q.popleft()
-                self.store.pop(temp)
-            self.q.append(key)
-        self.store[key] = value
+        if key in self.cache:
+            self.remove(self.cache[key])
+        self.cache[key] = Node(key, value)
+        self.insert(self.cache[key])
+
+        if len(self.cache) > self.cap:
+            lru = self.left.next
+            self.remove(lru)
+            self.cache.pop(lru.key)
 
 
-        
 # Your LRUCache object will be instantiated and called as such:
 # obj = LRUCache(capacity)
 # param_1 = obj.get(key)
