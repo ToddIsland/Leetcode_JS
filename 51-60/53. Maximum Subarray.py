feab47f9b348c9b37b7571a5fd64b4ee689291f4
Todd Hu <jkghdfas6@gmail.com>
2023-07-25 01:43:38 -0400

    Jul 25

---
+++ 51-60/53. Maximum Subarray.py
@@ -0,0 +1,11 @@
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        total = 0
+        res = nums[0]
+
+        for n in nums:
+            total += n
+            res = max(res, total)
+            if total < 0:
+                total = 0
+        return res
